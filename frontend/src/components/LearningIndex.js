4de43252c4482815731c7ad1783960bfb07f1466
JetB-commits <backup@jetb.co.jp>
2025-08-19 17:14:37 +0900

    test

--- frontend/src/components/LearningIndex.js
+++ frontend/src/components/LearningIndex.js
@@ -76,7 +76,7 @@ function LearningIndex() {
                             {item.sourceType && <div className="data-item-type">データ形式:{item.sourceType}</div>}
                             {item.sourceUrl && (
                                 <div className="data-item-url">
-                                    <a href={item.sourceUrl} target="_blank" rel="noreferrer nofollow">{item.sourceUrl}</a>
+                                    <a href={item.sourceUrl} target="_blank" rel="noreferrer nofollow" style={{ color: '#fff' }}>{item.sourceUrl}</a>
                                 </div>
                             )}
                         </div>
